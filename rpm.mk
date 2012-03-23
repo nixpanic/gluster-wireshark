e4ae753ece35c8c263b9d3ccb9740803722b1be5
Niels de Vos <ndevos@redhat.com>
2012-03-23 11:25:27 +0100

    Introduce a 'srpm' make target
    This target can be used to create an .src.rpm. Source RPM files can
    easily be built on other systems.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- rpm.mk
+++ rpm.mk
@@ -3,5 +3,10 @@ rpm: dist
 	$(mkinstalldirs) build/{SPECS,RPMS,BUILD,BUILDROOT,SRPMS}
 	rpmbuild --define "_topdir `pwd`/build" -ta $(DIST_ARCHIVES)
 
+.PHONY: srpm
+srpm: dist
+	$(mkinstalldirs) build/SRPMS
+	rpmbuild --define "_topdir `pwd`/build" -ts $(DIST_ARCHIVES)
+
 clean-local::
 	/bin/rm -rf build
